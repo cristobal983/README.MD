b6eb3075c1d083abbbf6d623e9aa1d3664c874db
cristobal983 <cristo.floresr@duocuc.cl>
2025-07-03 14:48:53 -0400

    Add files via upload

--- calculadora (1).py
+++ calculadora (1).py
@@ -1,6 +1,6 @@
 num1 = int(input("numero 1: ")) 
 num2 = int(input("numero 2: ")) 
-
+num3 = int(input("numero 3"))
 valor = 0
 while True:
     print("""seleccione opcion
@@ -15,15 +15,29 @@ while True:
     if valor == 1:
         print("la suma es",num1+num2)
         break;
-    if valor == 2:
+    elif valor == 2:
         print("la resta es",num1-num2)
         break;
-    if valor == 3:
+    elif valor == 3:
         print("la multiplicacion es",num1*num2)
         break;
-    if valor == 4:
+    elif valor == 4:
         print("la division es",num1/num2)
         break;
-    else:
-        print("Opcion incorrecta")
+    elif valor == 5:
+        print("la suma de 3 valores es", num1 + num2 + num3)
         break;
+    elif valor == 6:
+        print("La potencia es:" , num1**num2) 
+        break;
+    elif valor == 7:
+        print("Saliendo del programa")
+        break;
+    elif valor == 8:
+        print ("Hola mundo")
+    else:
+        print ("Opcion incorrecta")
+        break;
+            
+        
+       
